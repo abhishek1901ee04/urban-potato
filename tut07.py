065122b38a25256d603e8483cb969914de8f3edf
abhishek1901ee04 <54908876+abhishek1901ee04@users.noreply.github.com>
2021-11-23 12:21:35 +0530

    Update tut07.py

--- tut07.py
+++ tut07.py
@@ -38,32 +38,32 @@ with open("course_registered_by_all_students.csv", "r")as f:
         dct = dict(row)
         subject_data[dct["subno"]] = [dct["register_sem"], dct["schedule_sem"]]
 # print(subject_data)
-list = {}
-list1 = {}
+alloted_subject = {}
+feedback_given_subject = {}
 for i in student_rollno:
-    list[i] = []
-    list1[i] = []
+    alloted_subject[i] = []
+    feedback_given_subject[i] = []
 
 with open("course_registered_by_all_students.csv", "r")as f:
     reader = csv.DictReader(f)
     for row in reader:
         dct = dict(row)
-        if dct["rollno"] not in list:
-            list[dct["rollno"]] = []
-        list[dct["rollno"]].append(dct["subno"])
+        if dct["rollno"] not in alloted_subject:
+            alloted_subject[dct["rollno"]] = []
+        alloted_subject[dct["rollno"]].append(dct["subno"])
 
 with open("course_feedback_submitted_by_students.csv", "r")as f:
     reader = csv.DictReader(f)
     for row in reader:
         dct = dict(row)
+        temp=[]
+        if dct["stud_roll"] not in feedback_given_subject:
+            feedback_given_subject[dct["stud_roll"]] = []
+        temp.append(dct["course_code"])
+        temp.append(dct["feedback_type"])
+        feedback_given_subject[dct["stud_roll"]].append(temp)
 
-        if dct["stud_roll"] not in list1:
-            list1[dct["stud_roll"]] = []
-        list1[dct["stud_roll"]].append(dct["course_code"])
-        list1[dct["stud_roll"]].append(dct["feedback_type"])
-
-#  print(list)
-print(list1)
+#  print(alloted_subject)
 subject_count = {}
 with open("modified.csv", "r")as f:
     reader = csv.DictReader(f)
@@ -73,55 +73,53 @@ with open("modified.csv", "r")as f:
 # print(subject_count)
 data_xls = pd.read_excel(
     'course_feedback_remaining.xlsx', 'Sheet1')
-# d = Counter(list1)
+# d = Counter(feedback_given_subject)
 xx = []
-for i in list:
-    for subject in list[i]:
-        if i in list1:
-            if subject not in list1[i] and i in student_data and subject in subject_count:
-    			if subject_count[subject] == "1":
-				    data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-														student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-				if subject_count[subject] == "2":
-					data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-														student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-					data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-														student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-				if subject_count[subject] == "3":
-					data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-														student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-					data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-														student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-					data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-														student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-            else:
-                d = Counter(list1[i])
-                if d not in xx:
-                    xx.append(d)
-                    # print(int(subject_count[subject]), int(d[subject]))
-                    if(subject in subject_count and d[subject] != subject_count[subject] and i in student_data):
-                        t = abs(int(subject_count[subject]) - int(d[subject]))
-                        # print(t)
-                        while(t > 0):
-                            t -= 1
-                            data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-                                                                 student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+for i in alloted_subject:
+    for subject in alloted_subject[i]:
+        if i in feedback_given_subject:
+            for j in feedback_given_subject[i]:
+              if subject != j[0] and i in student_data and subject in subject_count:
 
-        # if t == 1:
-        #     # print("heyy1")
-        #     data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-        #                                          student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-        # if t == 2:
-        #     # print("heyy2")
-        #     data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-        #                                          student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
-        #     data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
-        #                                          student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+                if subject_count[subject] == "1":
+                  data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
+                                  student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+                if subject_count[subject] == "2":
+                  data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
+                                  student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+                  data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+                if subject_count[subject] == "3":
+                  data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
+                                  student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+                  data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
+                                  student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+                  data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
+                                  student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
+
+              
+              elif i not in student_data:
+                t="NA_IN_STUDENTINFO"
+                data_xls.loc[len(data_xls.index)]=[i,t,t,t,t,t,t,t]
+
+              # else:
+              #     d = Counter(feedback_given_subject[i])
+              #     print(type(d))
+              #     print(d)
+              #     if d not in xx:
+              #         xx.append(d)
+              #         print(int(subject_count[subject]), int(d[subject]))
+              #         if(subject in subject_count and d[subject] != subject_count[subject] and i in student_data):
+              #             t = abs(int(subject_count[subject]) - int(d[subject]))
+              #             # print(t)
+              #             while(t > 0):
+              #                 t -= 1
+              #                 data_xls.loc[len(data_xls.index)] = [i, subject_data[subject][0], subject_data[subject][1], subject,
+              #                                                     student_data[i][4], student_data[i][5], student_data[i][6], student_data[i][7]]
 
 
 # print(data_xls)
-file_name = 'MarksData.xlsx'
+# file_name = 'MarksData.xlsx'
 
-# saving the excel
-data_xls.to_excel(file_name)
+# # saving the excel
+# data_xls.to_excel(file_name)
 
